e3818d0d459d5788267c03707c62f9e38a13537b
huongw <whitneyhuong@gmail.com>
2021-08-25 22:04:48 +0000

    created a client.js file and added the connect function into that file and used ES6 syntax to export connect function and require it from play.js

---
+++ client.js
@@ -0,0 +1,22 @@
+const net = require('net');
+
+const connect = function () {
+  const conn = net.createConnection({ // used node's net library to create an object named conn
+    host: `135.23.223.133`,
+    port: 50542
+  });
+
+  conn.setEncoding(`utf8`);
+
+  conn.on('data', (data) => {
+    console.log(data)
+  })
+
+  return conn; // the conn object that node returned represents the connection with the server
+};
+
+console.log(`connecting...`);
+
+connect();
+
+module.exports = connect;
