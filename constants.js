d9b4d2fc59d3165d6c0c1f192ab5faf4a668395e
huongw <whitneyhuong@gmail.com>
2021-08-26 03:10:51 +0000

    moved constants inside of constants.js file and cleaned up code

---
+++ constants.js
@@ -0,0 +1,16 @@
+const IP = `135.23.223.133`;
+
+const PORT = 50542;
+
+const keyboard = {
+  w: `Move: up`,
+  a: `Move: left`,
+  s: `Move: down`,
+  d:`Move: right`
+};
+
+module.exports = {
+  IP,
+  PORT,
+  keyboard
+};
