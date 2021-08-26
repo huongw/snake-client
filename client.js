0c49dd8c27a630da49b7beb178cb96a3e0fcc34f
huongw <whitneyhuong@gmail.com>
2021-08-26 03:10:11 +0000

    fixed host and port section

--- client.js
+++ client.js
@@ -1,9 +1,10 @@
 const net = require('net');
+const {IP, PORT } = require("./constants");
 
 const connect = function () {
   const conn = net.createConnection({ // used node's net library to create an object named conn
-    host: `135.23.223.133`,
-    port: 50542
+    host: IP,
+    port: PORT
   });
 
   conn.setEncoding(`utf8`);
@@ -11,11 +12,11 @@ const connect = function () {
   conn.on(`connect`, () => {
     conn.write(`Name: WH`);
 
-    console.log(`I am connected`)
+    console.log(`I am connected`);
   })
 
   conn.on(`data`, (data) => {
-    console.log(data)
+    console.log(data);
   })
 
   return conn; // the conn object that node returned represents the connection with the server
